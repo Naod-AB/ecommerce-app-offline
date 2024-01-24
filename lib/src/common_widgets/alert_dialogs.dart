5ebdf210c2decc7fbb95f3d10031d1fabcf9e949
Naod <naodat.dev@gmail.com>
2024-01-24 11:01:12 +0300

    Implmented Go Router 13.0.1

--- lib/src/common_widgets/alert_dialogs.dart
+++ lib/src/common_widgets/alert_dialogs.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 /// Generic function to show a platform-aware Material or Cupertino dialog
 Future<bool?> showAlertDialog({
@@ -26,23 +27,21 @@ Future<bool?> showAlertDialog({
           ? <Widget>[
               if (cancelActionText != null)
                 TextButton(
-                  child: Text(cancelActionText),
-                  onPressed: () => Navigator.of(context).pop(false),
-                ),
+                    child: Text(cancelActionText),
+                    onPressed: () => context.pop(false)),
               TextButton(
-                child: Text(defaultActionText),
-                onPressed: () => Navigator.of(context).pop(true),
-              ),
+                  child: Text(defaultActionText),
+                  onPressed: () => context.pop(true)),
             ]
           : <Widget>[
               if (cancelActionText != null)
                 CupertinoDialogAction(
                   child: Text(cancelActionText),
-                  onPressed: () => Navigator.of(context).pop(false),
+                  onPressed: () => context.pop(false),
                 ),
               CupertinoDialogAction(
                 child: Text(defaultActionText),
-                onPressed: () => Navigator.of(context).pop(true),
+                onPressed: () => context.pop(true),
               ),
             ],
     ),
