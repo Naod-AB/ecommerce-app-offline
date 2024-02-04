6fb30d96967aec90d4c4b3cca8c07e18142914c5
Naod <naodat.dev@gmail.com>
2024-02-04 07:58:18 +0300

    signout implemented using Guard

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -15,14 +15,16 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   final FakeAuthRepository authoRepository;
 
   Future<bool> signOut() async {
-    try {
-      state = const AsyncValue<void>.loading();
-      await authoRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e, s) {
-      state = AsyncValue<void>.error(e, s);
-      return false;
-    }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authoRepository.signOut());
+    return state.hasError == false;
+// try {
+    //   await authoRepository.signOut();
+    //   state = const AsyncValue<void>.data(null);
+    //   return true;
+    // } catch (e, s) {
+    //   state = AsyncValue<void>.error(e, s);
+    //   return false;
+    // }
   }
 }
