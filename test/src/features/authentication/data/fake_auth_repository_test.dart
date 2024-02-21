4707f9c3a57c090142cb01f754a69f06aeb32017
Naod <naodat.dev@gmail.com>
2024-02-21 09:54:21 +0300

    Authentication for null user  implemented

--- test/src/features/authentication/data/fake_auth_repository_test.dart
+++ test/src/features/authentication/data/fake_auth_repository_test.dart
@@ -1,1 +1,22 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  const testEmail = 'test@test.com';
+  const testPassword = '1234';
+  final testUser = AppUser(
+    uid: testEmail.split('').reversed.join(),
+    email: testEmail,
+  );
+  FakeAuthRepository makeAuthRepository() => FakeAuthRepository(
+        addDelay: false,
+      );
+  group('Fake AUth Repository', () {
+    test('current user is null', () {
+      final authRepository = makeAuthRepository();
+      expect(authRepository.currentUser, null);
+      expect(authRepository.authStateChanges(), emits(null));
+    });
+  });
+}
