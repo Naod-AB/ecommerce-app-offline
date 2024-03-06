efa314d22a57c235ad7a7cdb12ef9500705c2ed7
Naod <naodat.dev@gmail.com>
2024-03-06 11:34:37 +0300

    refactored authentication test

--- test/src/features/authentication/auth_robot.dart
+++ test/src/features/authentication/auth_robot.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
@@ -42,6 +43,20 @@ class AuthRobot {
     );
   }
 
+  Future<void> tapEmailAndPasswordSubmitButton() async {
+    final primaryButton = find.byType(PrimaryButton);
+    expect(primaryButton, findsOneWidget);
+    await tester.tap(primaryButton);
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> tapFormToggleButton() async {
+    final toggleButton = find.byType(CustomTextButton);
+    expect(toggleButton, findsOneWidget);
+    await tester.tap(toggleButton);
+    await tester.pumpAndSettle();
+  }
+
   Future<void> enterEmail(String email) async {
     final emailField = find.byKey(EmailPasswordSignInScreen.emailKey);
     expect(emailField, findsOneWidget);
@@ -54,15 +69,19 @@ class AuthRobot {
     await tester.enterText(passwordField, password);
   }
 
-  Future<void> tapEmailAndPasswordSubmitButton() async {
-    final primaryButton = find.byType(PrimaryButton);
-    expect(primaryButton, findsOneWidget);
-    await tester.tap(primaryButton);
-    await tester.pumpAndSettle();
+  void expectCreateAccountButtonFound() {
+    final dialogTitle = find.text('Create an account');
+    expect(dialogTitle, findsOneWidget);
+  }
+
+  void expectCreateAccountButtonNotFound() {
+    final dialogTitle = find.text('Create an account');
+    expect(dialogTitle, findsNothing);
   }
 
   Future<void> signInWithEmailAndPassword() async {
     await enterEmail('test@test.com');
+    await tester.pump();
     await enterPassword('test1234');
     await tapEmailAndPasswordSubmitButton();
   }
@@ -80,7 +99,9 @@ class AuthRobot {
       ProviderScope(
         overrides: [
           if (authRepository != null)
-            authRepositoryProvider.overrideWithValue(authRepository)
+            authRepositoryProvider.overrideWithValue(
+              authRepository,
+            )
         ],
         child: const MaterialApp(
           home: AccountScreen(),
